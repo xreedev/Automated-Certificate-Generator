5a1a4db6a94260c04498eefabe999d9ac2007a6a
sreedev <xreedev@gmail.com>
2024-05-17 23:15:33 +0530

    added comments on main

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import extractExcel as EX
 import updateWord as UW
 import os
 from docx2pdf import convert
-student_files=os.listdir("student_data")
+student_files=os.listdir("student_data") #extract all csv files
 for file in student_files:
- student_list=EX.extractData(f"student_data\\{file}")
- UW.addInfo(student_list,"template.docx")
+ student_list=EX.extractData(f"student_data\\{file}") #extract data from csv file and store in list
+ UW.addInfo(student_list,"template.docx") #add info to template
 
-file_list=os.listdir("Final_Word")
+file_list=os.listdir("Final_Word")#take each docx final file
 for file in file_list:
-  convert(f"Final_Word\\{file}",output_path="Final_Certificates\\Sports")
+  convert(f"Final_Word\\{file}",output_path="Final_Certificates\\Sports") #convert to pdf
 
 
 
